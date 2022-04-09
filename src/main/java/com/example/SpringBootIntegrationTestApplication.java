f592fe14c634b8dc2b8d598daab0404724441c2c
Hendi Santika <hendisantika@gmail.com>
2022-04-09 10:03:40 +0700

    #7 - Add EmployeeController

--- src/main/java/com/example/SpringBootIntegrationTestApplication.java
+++ src/main/java/com/example/SpringBootIntegrationTestApplication.java
@@ -1,14 +1,18 @@
 package com.example;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.util.List;
 
 @SpringBootApplication
 public class SpringBootIntegrationTestApplication {
@@ -32,3 +36,15 @@ class Employee {
 @Repository
 interface EmployeeRepository extends JpaRepository<Employee, Long> {
 }
+
+@RestController
+@AllArgsConstructor
+class EmployeeController {
+    private EmployeeRepository employeeRepository;
+
+    @GetMapping("/employees")
+    public List<Employee> getEmployees() {
+        return employeeRepository.findAll();
+    }
+}
+
