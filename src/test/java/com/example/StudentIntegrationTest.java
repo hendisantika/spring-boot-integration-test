6c3a3dd27089f6911b173d0a00a82c3b85dfede5
Hendi Santika <hendisantika@gmail.com>
2022-04-09 10:39:39 +0700

    #24 - Add StudentIntegrationTest

---
+++ src/test/java/com/example/StudentIntegrationTest.java
@@ -0,0 +1,27 @@
+package com.example;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-integration-test
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 09/04/22
+ * Time: 10.39
+ */
+@SpringBootTest
+@AutoConfigureMockMvc
+public class StudentIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper; //we will use this later to convert the Student object to a json string
+}
