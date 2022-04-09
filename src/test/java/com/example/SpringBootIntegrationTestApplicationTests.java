9994409980cde0c15fae3a017388978b09d034e4
Hendi Santika <hendisantika@gmail.com>
2022-04-09 10:06:21 +0700

    #9 - Set Postgres Container

--- src/test/java/com/example/SpringBootIntegrationTestApplicationTests.java
+++ src/test/java/com/example/SpringBootIntegrationTestApplicationTests.java
@@ -1,10 +1,27 @@
 package com.example;
 
+import org.junit.ClassRule;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.testcontainers.containers.PostgreSQLContainer;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ContextConfiguration(initializers = {SpringBootIntegrationTestApplicationTests.Initializer.class})
 class SpringBootIntegrationTestApplicationTests {
+    @ClassRule
+    public static PostgreSQLContainer postgres = new PostgreSQLContainer("postgres")
+            .withDatabaseName("postgres")
+            .withUsername("postgres")
+            .withPassword("docker");
+
+    @Value("http://localhost:${local.server.port}")
+    String baseUrl;
+
+    @Autowired
+    EmployeeRepository employeeRepository;
 
     @Test
     void contextLoads() {
