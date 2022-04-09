03a69013d7ba5c0a81aa9aeaa0d1aa59656357f7
Hendi Santika <hendisantika@gmail.com>
2022-04-09 10:35:53 +0700

    #16 - Add StudentServiceImpl

---
+++ src/main/java/com/example/StudentServiceImpl.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-integration-test
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 09/04/22
+ * Time: 10.35
+ */
+@Service
+public class StudentServiceImpl implements StudentService {
+
+    @Autowired
+    private StudentRepository studentRepository;
+}
