08adac013c59faea1c6587f091b7e37a7c678bde
Hendi Santika <hendisantika@gmail.com>
2022-04-09 10:38:32 +0700

    #22 - Add findByName in StudentController

--- src/main/java/com/example/StudentController.java
+++ src/main/java/com/example/StudentController.java
@@ -2,8 +2,10 @@ package com.example;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,5 +28,11 @@ public class StudentController {
     @ResponseBody
     public Student save(@RequestBody Student student) {
         return studentService.save(student);
+    }
+
+    @GetMapping(value = {"/student"}, produces = {MediaType.APPLICATION_JSON_VALUE})
+    @ResponseBody
+    public Student findByName(@RequestParam String name) {
+        return studentService.findByName(name);
     }
 }
