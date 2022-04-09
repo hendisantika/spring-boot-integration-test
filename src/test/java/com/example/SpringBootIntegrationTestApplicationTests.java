a78e5e726ae21c424bd1caa7a94a88c5f98d29c9
Hendi Santika <hendisantika@gmail.com>
2022-04-09 10:06:52 +0700

    #10 - Add testWriteToDb_afterBoot_shouldHaveEntries Test Case

--- src/test/java/com/example/SpringBootIntegrationTestApplicationTests.java
+++ src/test/java/com/example/SpringBootIntegrationTestApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import org.assertj.core.api.Assertions;
 import org.junit.ClassRule;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.testcontainers.containers.PostgreSQLContainer;
+
+import java.util.List;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ContextConfiguration(initializers = {SpringBootIntegrationTestApplicationTests.Initializer.class})
@@ -27,4 +30,11 @@ class SpringBootIntegrationTestApplicationTests {
     void contextLoads() {
     }
 
+    @Test
+    public void testWriteToDb_afterBoot_shouldHaveEntries() {
+        List<Employee> all = employeeRepository.findAll();
+        Assertions.assertThat(all.size()).isEqualTo(6);
+        Assertions.assertThat(all.get(0).getFirstName()).isEqualTo("First");
+        Assertions.assertThat(all.get(0).getLastName()).isEqualTo("Last");
+    }
 }
