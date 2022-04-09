1a484f98451c9aecb4ed53a9dd2ca43d6f8d1c01
Hendi Santika <hendisantika@gmail.com>
2022-04-09 10:04:16 +0700

    #8 - Add Init Data

--- src/main/java/com/example/SpringBootIntegrationTestApplication.java
+++ src/main/java/com/example/SpringBootIntegrationTestApplication.java
@@ -2,8 +2,10 @@ package com.example;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.util.ArrayList;
 import java.util.List;
 
 @SpringBootApplication
@@ -21,6 +24,28 @@ public class SpringBootIntegrationTestApplication {
         SpringApplication.run(SpringBootIntegrationTestApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner commandlineRunner(EmployeeRepository employeeRepository) throws Exception {
+
+        return args -> {
+            employeeRepository.deleteAllInBatch();
+            ArrayList<Employee> employees = new ArrayList<>();
+            employees.add(createEmployee("First", "Last"));
+            employees.add(createEmployee("Mr.", "Frost"));
+            employees.add(createEmployee("Santa", "Clause"));
+            employees.add(createEmployee("Peter", "Pan"));
+            employees.add(createEmployee("Cinder", "ella"));
+            employees.add(createEmployee("What", "ever"));
+            employeeRepository.saveAll(employees);
+        };
+    }
+
+    private Employee createEmployee(String firstName, String lastName) {
+        Employee employee = new Employee();
+        employee.setFirstName(firstName);
+        employee.setLastName(lastName);
+        return employee;
+    }
 }
 
 @Data
